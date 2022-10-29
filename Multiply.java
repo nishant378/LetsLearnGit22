1d258db542cbd12e9dc6d9d00cc73e967a4cc0b6
Nishant Kumar <nk5374890@gmail.com>
2022-10-29 17:58:55 +0530

    Updated:Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -4,7 +4,8 @@ public class Array1 {
 
 	public static void main(String[] args) {
 		int a=10;
-        int b=5;
-       System.out.println(a*b);
+                int b=5;
+                System.out.println(a*b);
+                System.out.println(a*b);
 	}
 }
